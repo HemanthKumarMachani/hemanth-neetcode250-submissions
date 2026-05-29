37b3b5dad34431e65473c7947fc2b1b520945130
HemanthKumarMachani <63578075+HemanthKumarMachani@users.noreply.github.com>
2026-05-29 18:31:14 -0500

    Add: arranging-coins - submission-3

---
+++ Data Structures & Algorithms/arranging-coins/submission-3.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int arrangeCoins(int n) {
+        int low = 0, high = n;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            long coins = (long) mid * (mid + 1) / 2;
+            if(coins == n) return mid;
+            else if(coins < n){
+                low = mid + 1;
+            }else{
+                high = mid - 1;
+            }
+        }
+        return high;
+    }
+}
