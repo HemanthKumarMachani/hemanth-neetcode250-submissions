62172322c875bfb093a3c52b58eb86a71796ed2a
HemanthKumarMachani <63578075+HemanthKumarMachani@users.noreply.github.com>
2026-06-04 00:00:00 -0500

    Add: search-2d-matrix - submission-10

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-10.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int rows = matrix.length, cols = matrix[0].length;
+        int low = 0, high = rows * cols - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int midVal = matrix[mid / cols][mid % cols];
+            if (midVal == target)
+                return true;
+            else if (midVal < target)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return false;
+    }
+}
