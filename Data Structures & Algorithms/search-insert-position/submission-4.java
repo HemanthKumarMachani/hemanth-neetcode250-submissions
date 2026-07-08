ec65b658606dfe66fcfc8bd96338c22f66647063
HemanthKumarMachani <63578075+HemanthKumarMachani@users.noreply.github.com>
2026-07-07 21:42:01 -0500

    Add: search-insert-position - submission-4

---
+++ Data Structures & Algorithms/search-insert-position/submission-4.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        //input validation
+        if(nums == null || nums.length == 0) return 0;
+        int low = 0, high = nums.length;
+        while(low < high){
+            int mid = low + (high - low)/ 2;
+            if(nums[mid] == target){
+                return mid;
+            }else if(nums[mid] < target){
+                low = mid + 1;
+            }else{
+                high = mid;
+            }
+        }
+        return high;
+    }
+}
