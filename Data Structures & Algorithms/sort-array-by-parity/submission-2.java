4ccdda197e21f452faff79656f21163ecca17d32
HemanthKumarMachani <63578075+HemanthKumarMachani@users.noreply.github.com>
2026-07-22 13:36:28 -0400

    Add: sort-array-by-parity - submission-2

---
+++ Data Structures & Algorithms/sort-array-by-parity/submission-2.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        // input validation
+        if (nums == null)
+            return nums;
+        int evenIdx = 0;
+        for (int index = 0; index < nums.length; index++) {
+            if ((nums[index] & 1) == 0) {
+                int temp = nums[evenIdx];
+                nums[evenIdx++] = nums[index];
+                nums[index] = temp;
+            }
+        }
+        return nums;
+    }
+}
